40f41f97c3f9e6c91c99cf8c288ed4bf3564025b
acidin <kartsan@ya.ru>
2016-08-01 15:56:07 +0300

    add static events source

--- README.md
+++ README.md
@@ -1,23 +1,5 @@
-# Todo 演示项目
-
-本项目依赖 mongodb, 以 OS X 系统为例, 可以通过 brew 安装 [mongodb](https://www.mongodb.org/)
-
-```shell
-brew install mongodb
-```
-
-然后启动 mongodb:
-
-```shell
-mongod --config /usr/local/etc/mongod.conf
-```
-
-### 运行步骤
+DEVELOPMENT
 
 * npm install
 
 * npm run start
-
-### 生成静态文件
-
-* npm run deploy
--- app/index/containers/App.js
+++ app/index/containers/App.js
@@ -3,7 +3,19 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import Header from '../components/Header'
 import MainSection from '../components/MainSection'
+import BigCalendar from 'react-big-calendar';
 import * as TodoActions from '../actions'
+
+import events from '../events';
+
+import moment from 'moment';
+
+import '../less/styles.less';
+import '../less/prism.less';
+
+BigCalendar.setLocalizer(
+    BigCalendar.momentLocalizer(moment)
+);
 
 class App extends Component {
 
@@ -18,6 +30,12 @@ class App extends Component {
       <div>
         <Header addTodo={actions.addTodo}/>
         <MainSection todos={todos} actions={actions} isFetching={isFetching}/>
+          <BigCalendar
+              defaultView='week'
+              events={events}
+              defaultDate={new Date(2015, 3, 1)}
+              views={['week']}
+          />
       </div>
     )
   }
